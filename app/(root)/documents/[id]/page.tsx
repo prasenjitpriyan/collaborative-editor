56685c94e80d2c734580458d756e04b0334974e1
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-21 23:26:02 +0530

    Editor implemented

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,12 @@
+import { Editor } from "@/components/editor/Editor";
 import React from "react";
 
 const Document: React.FC = () => {
-  return <div>Document</div>;
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
 };
 
 export default Document;
