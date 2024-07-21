ff22f85cbd65d0363344bea17cb54283a9ef6861
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-21 22:38:25 +0530

    Initial setup complete

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Metadata } from "next";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "LiveDocs is a collaborative editor for live documents.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
