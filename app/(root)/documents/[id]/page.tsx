b461014879bfd107c83ea0804e855d14c82fa962
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-22 19:16:21 +0530

    Clerk added

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,22 @@
-import { Editor } from "@/components/editor/Editor";
 import React from "react";
+import { Editor } from "@/components/editor/Editor";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const Document: React.FC = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Title</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
       <Editor />
     </div>
   );
