d4a3b16e294051847bd48e9ae076dcd2b9bc917f
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-22 20:03:02 +0530

    Loader component added to the provider

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,13 +5,12 @@ import {
   LiveblocksProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
-        {children}
-      </ClientSideSuspense>
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
