832bd8a8e5c93f02b8cb1a4b7c56e6c7c7cca8c1
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-24 07:15:03 +0530

    global error page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import "./globals.css";
 import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Provider from "./Provider";
 
@@ -14,7 +14,7 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: "LiveDocs",
-  description: "LiveDocs is a collaborative editor for live documents.",
+  description: "Your go-to collaborative editor",
 };
 
 export default function RootLayout({
@@ -26,11 +26,13 @@ export default function RootLayout({
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
       }}
     >
       <html lang="en" suppressHydrationWarning>
-        <head />
         <body
           className={cn(
             "min-h-screen font-sans antialiased",
